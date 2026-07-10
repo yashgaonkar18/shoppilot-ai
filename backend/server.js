8b5d5c027a60488919ecef3c4749fb9e7ec22c92
yashgaonkar18 <yashjr1818@gmail.com>
2026-07-10 22:17:15 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,6 @@ import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
 
-app.use((req, res, next) => {
-  const start = Date.now();
-  console.log(`➡️  ${req.method} ${req.originalUrl}`);
-  
-  res.on("finish", () => {
-    const duration = Date.now() - start;
-    console.log(`⬅️  ${req.method} ${req.originalUrl} - ${res.statusCode} (${duration}ms)`);
-  });
-  
-  next();
-});
 
 // Routes
 import authRoutes from "./routes/auth.routes.js";
@@ -39,6 +28,17 @@ await connectDB();
 startReorderJob();
 startInventoryJob();
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  console.log(`➡️  ${req.method} ${req.originalUrl}`);
+  
+  res.on("finish", () => {
+    const duration = Date.now() - start;
+    console.log(`⬅️  ${req.method} ${req.originalUrl} - ${res.statusCode} (${duration}ms)`);
+  });
+  
+  next();
+});
 // Middlewares
 app.use(cors({
   origin: [
